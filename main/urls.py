f267b24673e542df9a3c2e885b697f4802f4f9fe
AdamMoh <xlkartu12345@gmail.com>
2023-09-26 22:01:19 +0700

    Assignment 4 with bonus

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product,show_xml,show_json,show_xml_by_id,show_json_by_id
+from main.views import show_main, create_product,show_xml,show_json,show_xml_by_id,show_json_by_id, register, login_user, logout_user, delete_item, increment_item, decrement_item
 
 app_name = 'main'
 
@@ -9,5 +9,11 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('delete/<int:id>', delete_item, name='delete_item'),
+    path('increment/<int:id>', increment_item, name='increment_item'),
+    path('decrement/<int:id>', decrement_item, name='decrement_item'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,11 +5,20 @@ from main.forms import ItemForm
 from django.http import HttpResponse
 from django.core import serializers
 from django.urls import reverse
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
 
+@login_required(login_url='/login')
 def show_main(request):
 
-    dataAll = Item.objects.all()
+    dataAll = Item.objects.filter(user=request.user)
     dataCount = dataAll.count()
     
     context = {
@@ -18,6 +27,7 @@ def show_main(request):
         'class': 'KKI',
         'datas': dataAll,
         'dataCounts' : dataCount,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, 'main.html', context)
@@ -26,7 +36,9 @@ def create_product(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
@@ -47,3 +59,64 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def delete_item(request, id):
+    try:
+        data = Item.objects.get(id=id)
+        data.delete()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    except Item.DoesNotExist:
+        return HttpResponse(status=204)
+    
+def increment_item(request, id):
+    data = Item.objects.get(id=id)
+    data.amount += 1
+    data.save()
+
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def decrement_item(request, id):
+    data = Item.objects.get(id=id)
+    data.amount -= 1
+
+    if data.amount == 0:
+        data.delete()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    else:
+        data.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    
